4b96f6c0a1622fc747cc33e73381b5e1f1ffdee5
ludovitkovac <ludo.kovac@gmail.com>
2016-08-10 23:48:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -25,52 +19,38 @@ and open the template in the editor.
             die("Connection failed: " . $conn->connect_error);
         }
         echo "Connected successfully<br><br>";
+        $sum = 0;
+        for ($i = 1; $i < 50; $i++) {
 
-        $sql = "SELECT * FROM tah1";
-        $result = $conn->query($sql);
-        
-        echo 'PRVÝ ŤAH: <br><br>';
+            $sql1 = "SELECT COUNT(id) AS pocet FROM `tah1` WHERE cislo1=$i OR cislo2=$i OR cislo3=$i OR cislo4=$i OR cislo5=$i OR cislo6=$i OR cislo7=$i";
 
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                
-                echo "Dátum: " . $row["datum"] . "<br> Čísla: " . 
-                        $row["cislo1"] . " - " . 
-                        $row["cislo2"] . " - " . 
-                        $row["cislo3"] . " - " . 
-                        $row["cislo4"] . " - " . 
-                        $row["cislo5"] . " - " . 
-                        $row["cislo6"] . " - " . 
-                        $row["cislo7"] . "<br><br>";
-                
+            $result1 = $conn->query($sql1);
+
+            if ($result1->num_rows > 0) {
+                // output data of each row
+                while ($row = $result1->fetch_assoc()) {
+
+                    $tah1 = $row["pocet"];
+                    $sum += $tah1;
+                }
             }
-        } else {
-            echo "0 results";
-        }
-        
-        $sql = "SELECT * FROM tah2";
-        $result = $conn->query($sql);
-        
-        echo 'DRUHÝ ŤAH: <br><br>';
 
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                
-                echo "Dátum: " . $row["datum"] . "<br> Čísla: " . 
-                        $row["cislo1"] . " - " . 
-                        $row["cislo2"] . " - " . 
-                        $row["cislo3"] . " - " . 
-                        $row["cislo4"] . " - " . 
-                        $row["cislo5"] . " - " . 
-                        $row["cislo6"] . " - " . 
-                        $row["cislo7"] . "<br><br>";
-                
+            $sql2 = "SELECT COUNT(id) AS pocet FROM `tah2` WHERE cislo1=$i OR cislo2=$i OR cislo3=$i OR cislo4=$i OR cislo5=$i OR cislo6=$i OR cislo7=$i";
+            $result2 = $conn->query($sql2);
+            if ($result2->num_rows > 0) {
+                // output data of each row
+                while ($row = $result2->fetch_assoc()) {
+
+                    $tah2 = $row["pocet"];
+                    $sum += $tah2;
+                }
             }
-        } else {
-            echo "0 results";
+
+            echo "Počet vyžrebovaní čísla " . $i . " je " . ($tah1 + $tah2) . "<br>";
         }
+
+        echo "<br>Počet všetkých vyžrebovaných čísel je " . $sum;
+
         $conn->close();
         ?>
     </body>
